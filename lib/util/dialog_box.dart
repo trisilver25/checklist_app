10287d2a98e2ea8f267e73403fb8c467c43f39ed
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2024-11-07 19:27:09 -0500

    Initial Commit

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,51 @@
+import 'package:checklist_app/util/my_button.dart';
+import 'package:flutter/material.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key, 
+    required this.controller,
+    required this.onCancel,
+    required this.onSave,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: SizedBox(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          // get user input
+          TextField(
+            controller: controller,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: "Add a new task",
+            ),
+          ),
+
+          // buttons -> save + cancel
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // save button
+              MyButton(text: "Save", onPressed: onSave),
+
+              const SizedBox(width: 8),
+              // cancel button
+              MyButton(text: "Cancel", onPressed: onCancel),
+          ],
+         ),
+        ]),
+      ),
+    );
+  }
+}
